2a1e3e0617c117c7a5eaaa903f8e24c7bd2c615c
luiz-couto <luiz.felipe.556@outlook.com>
2023-04-29 14:18:41 -0300

    removed old gunrock lib

---
+++ iwp-gunrock.cu
@@ -0,0 +1,10 @@
+#include <gunrock/algorithms/algorithms.hxx>
+
+using namespace gunrock;
+using namespace memory;
+
+int main()
+{
+    io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+    return 0;
+}
