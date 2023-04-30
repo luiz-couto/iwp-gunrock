96517bf7f9bc64402a33e0ea9e28d0c740c0f84c
luiz-couto <luiz.felipe.556@outlook.com>
2023-04-30 20:19:11 -0300

    gunrock configured

--- iwp-gunrock.cu
+++ iwp-gunrock.cu
@@ -1,10 +1,16 @@
 #include <gunrock/algorithms/algorithms.hxx>
+#include <gunrock/formats/formats.hxx>
 
 using namespace gunrock;
 using namespace memory;
 
 int main()
 {
-    io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+    using vertex_t = int;
+    using edge_t = int;
+    using weight_t = float;
+
+    using csr_t = format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
+
     return 0;
 }
