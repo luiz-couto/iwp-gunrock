cf034ae5728716dac52e21f5dbfd741b0f283958
luiz-couto <luiz.felipe.556@outlook.com>
2023-05-01 15:15:25 -0300

    added opencv

--- iwp-gunrock.cu
+++ iwp-gunrock.cu
@@ -1,8 +1,11 @@
-#include <gunrock/algorithms/algorithms.hxx>
+// #include <gunrock/algorithms/algorithms.hxx>
 #include <gunrock/formats/formats.hxx>
+#include <iostream>
+#include <filesystem>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 
-using namespace gunrock;
-using namespace memory;
+namespace fs = std::filesystem;
 
 int main()
 {
@@ -10,7 +13,19 @@ int main()
     using edge_t = int;
     using weight_t = float;
 
-    using csr_t = format::csr_t<memory_space_t::device, vertex_t, edge_t, weight_t>;
+    std::cout << "Current path is " << std::filesystem::current_path() << '\n';
+
+    using csr_t = gunrock::format::csr_t<gunrock::memory_space_t::device, vertex_t, edge_t, weight_t>;
+
+    std::string image_path = cv::samples::findFile("karu2.jpg");
+    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+
+    std::cout << img.size().width << std::endl;
 
     return 0;
 }
